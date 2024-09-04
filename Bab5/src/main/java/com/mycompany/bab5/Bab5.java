361f8eb67d35abfc4ecc1632bec7c3b33d0447b2
alika1907 <141797171+alika1907@users.noreply.github.com>
2024-09-03 21:20:12 -0400

    Add files via upload
    MODUL 5

---
+++ Bab5/src/main/java/com/mycompany/bab5/Bab5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab5;
+
+/**
+ *
+ * @author RPL-SMKN 4 BOGOR
+ */
+public class Bab5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
